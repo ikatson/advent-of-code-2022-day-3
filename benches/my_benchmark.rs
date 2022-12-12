ce6abaf99e1e28e1217ef581f1e41f61e8677d06
Igor Katson <igor.katson@gmail.com>
2022-12-12 09:00:43 +0000

    pre-split?

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -7,6 +7,11 @@ fn criterion_benchmark(c: &mut Criterion) {
     let mut f = std::fs::File::open("input.txt").unwrap();
     let mut s = String::new();
     f.read_to_string(&mut s).unwrap();
+
+    let lines: Vec<Vec<u8>> = ad3p2::lsplit::l2(s.as_bytes())
+        .map(|l| l.to_owned())
+        .collect();
+
     c.bench_function("process_buf::s1", |b| {
         b.iter(|| s1::process_buf(black_box(s.as_bytes())))
     });
@@ -24,6 +29,24 @@ fn criterion_benchmark(c: &mut Criterion) {
     });
     c.bench_function("process_buf::s3_part2", |b| {
         b.iter(|| s3::process_buf_part_2(black_box(s.as_bytes())))
+    });
+
+    c.bench_function("process_buf::s3_part2 aligned with c3", |b| {
+        b.iter(|| {
+            ad3p2::part2::process_buf_generic(
+                lines.iter().map(|l| l.as_ref()),
+                ad3p2::compartment::c3,
+            )
+        });
+    });
+
+    c.bench_function("process_buf::s3_part2 aligned with simd c2", |b| {
+        b.iter(|| {
+            ad3p2::part2::process_buf_generic(
+                lines.iter().map(|l| l.as_ref()),
+                ad3p2::compartment::c2,
+            )
+        });
     });
 }
 
